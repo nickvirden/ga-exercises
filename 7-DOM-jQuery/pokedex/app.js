4ce005faa58769da8e510308984b25881f576656
Nick Virden <nickvirden@gmail.com>
2016-07-15 02:22:29 +0000

    Class 7

--- 7-DOM-jQuery/pokedex/app.js
+++ 7-DOM-jQuery/pokedex/app.js
@@ -1,23 +1,38 @@
-// console.log("hello from app.js")
-
-var pokemonNameField = document.getElementById('pokemon-name');
-var addPokemonBtn = document.getElementById('add-pokemon');
-var pokemonList = document.getElementById('pokemon-list');
-
-console.log(pokemonNameField);
-console.log(addPokemonBtn);
-console.log(pokemonList);
-
-addPokemonBtn.onclick = function() {
-    // Creates a new list item every time you click the button
-    var newListItem = document.createElement('li');
+//jQuery
+$(function() {
+    var $pokemonNameField = $('#pokemon-name'),
+        $addPokemonBtn = $('#add-pokemon'),
+        $pokemonList = $('#pokemon-list');
     
-    // The list item includes the inner HTML in the element
-    newListenItem.innerHTML = pokemonNameField.value;
+    $addPokemonBtn.on('click', function() {
+        var listItem = $pokemonList.append('<li>' + $pokemonList.val() + '</li>');
+        $pokemonList.append(listItem);
+    })
     
-    // Adds new child to the list item
-    pokemonList.appendChild(newListItem);
+});
+
+//Pure JavaScript
+/*window.onload = function() {
     
-    // Make field empty on click
-    pokemonNameField.value = "";
-}
+    var pokemonNameField = document.getElementById('pokemon-name');
+    var addPokemonBtn = document.getElementById('add-pokemon');
+    var pokemonList = document.getElementById('pokemon-list');
+
+    console.log(pokemonNameField);
+    console.log(addPokemonBtn);
+    console.log(pokemonList);
+
+    addPokemonBtn.onclick = function() {
+        // Creates a new list item every time you click the button
+        var newListItem = document.createElement('li');
+
+        // The list item includes the inner HTML in the element
+        newListenItem.innerHTML = pokemonNameField.value;
+
+        // Adds new child to the list item
+        pokemonList.appendChild(newListItem);
+
+        // Make field empty on click
+        pokemonNameField.value = "";
+    };
+};*/
